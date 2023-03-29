c5c92ef7a2a5c60f515abcf313f4645e386f5e3d
Makhan AutomationHub <79011947+mak1985@users.noreply.github.com>
2023-03-29 11:55:20 +0530

    Core_java_Codes

---
+++ session02/CheckDefaultValueOfPrimitiveDataTypes.java
@@ -0,0 +1,11 @@
+package session02;
+
+public class CheckDefaultValueOfPrimitiveDataTypes {
+
+    static String a;
+
+    public static void main(String[] args) {
+
+        System.out.println(a);
+    }
+}
---
+++ session02/DataVariables.java
@@ -0,0 +1,26 @@
+package session02;
+
+public class DataVariables {
+
+    public static void main(String[] args) {
+
+        // 1. Declaration of a variable
+        int a;
+
+        //2. Initialization of a variable
+        a = 10;
+        a = 20; // Re-assign the value
+
+
+        // Declaration + Initialization
+        int x = 10;
+        x = 5;  // Re-assign the value
+
+//        String empName = "John";
+//        double salary = 10000.99;
+
+        System.out.println(x);//10
+        System.out.println(a);//10
+
+    }
+}
