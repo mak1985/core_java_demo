58f0031877f5bef16f275ec740df6ea604f785bb
Makhan AutomationHub <makhan.dhar85@gmail.com>
2023-03-30 10:34:42 +0530

    Initial code

---
+++ src/session03/LogicalOperators.java
@@ -0,0 +1,20 @@
+package session03;
+
+public class LogicalOperators {
+    public static void main(String[] args) {
+
+        //4) Logical operator - ( &&, ||, ! ) - true or false
+
+        boolean x = false;
+        boolean y = true;
+
+
+
+
+
+        System.out.println(x && y);//false
+        System.out.println(x || y);//true
+        System.out.println(!x); //true
+        System.out.println(!y);//false
+    }
+}
